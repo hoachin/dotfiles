eb01679af3f9db6ee67a82df131b7a2c0046f3f9
yaldi <g@yaldi.co>
2024-03-18 14:29:01 +0000

    Add indent-blankline plugin

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    exclude = {
+      filetypes = {
+        "dashboard",
+        "mason",
+        "notify",
+        "help",
+      },
+    },
+  },
+}
