e394887c457b787348c163e6e63c5e5d8c9e1ed8
yaldi <g@yaldi.co>
2024-08-04 20:32:55 +0100

    Add additional entires to dashboard

--- .config/nvim/lua/plugins/dashboard-nvim.lua
+++ .config/nvim/lua/plugins/dashboard-nvim.lua
@@ -11,6 +11,11 @@ return {
         enable = true
       },
       center = {
+        { action = ':Telescope find_files', desc = " Find File", icon = " ", key = "f" },
+        { action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
+        { action = ':Telescope oldfiles', desc = " Recent Files", icon = " ", key = "r" },
+        { action = ':Telescope live_grep', desc = " Find Text", icon = " ", key = "g" },
+        { action = ":Lazy", desc = "Plugins", icon = " ", key = "p" },
         { action = ":qa", desc = "Quit", icon = " ", key = "q" },
       },
       footer = {},
